584dee46b31163ec5e884c1c555021f347711d30
rszrama <rszrama>
2007-03-19 22:59:42 +0000

    Initial upload of Ubercart, woohoo!

---
+++ uc_cart/uc_cart_block.js
@@ -0,0 +1,23 @@
+// $Id: uc_cart_block.js,v 1.1 2007-03-19 22:59:42 rszrama Exp $
+
+/**
+ * Collapse the shopping cart block at page load.
+ */
+$(document).ready(function(){
+  $('#block-cart-contents').hide(0);
+});
+
+/**
+ * Toggle the shopping cart block open and closed.
+ */
+function cart_toggle(uc_cart_path) {
+  $('#block-cart-contents').toggle();
+
+  isrc = $('#block-cart-title-arrow').attr('src');
+  if (isrc.toLowerCase().match("up") != null) {
+    $('#block-cart-title-arrow').attr('src', uc_cart_path + '/images/bullet-arrow-down.png');
+  }
+  else {
+    $('#block-cart-title-arrow').attr('src', uc_cart_path + '/images/bullet-arrow-up.png');
+  }
+}                                                                             
