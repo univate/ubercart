ed0a14627a380a9a7c88f281f4e57278bb30bd5f
islandusurper <islandusurper>
2009-09-23 15:45:55 +0000

    2009-09-23 Changes since Ubercart 6.x-2.x-dev 2009-09-21 (-r 1958..1968):
    d#566510 by CrashTest_, rszrama (Ryan): make the update message for add to cart form submissions optional through a global variable.
    Fixing a regression in the checkout messages that caused \n to be escaped.
    d#467632 by IslandUsurper (Lyle): make uc_currency_format() into a generic wrapper for uc_price().
    Fix regression caused by 378364: Remove query to delete all products when order is saved.
    #450500 by cha0s and Island Usurper: Adjust stock levels when products are added to and removed from orders.
    Forward porting the custom product report from Ubercart 1.x with date and order status filters.
    d#515128 by lop: detect zone ID for countries other than US when payment accepted by PayPal Express Checkout.
    Fix uc_product_kit_buy_it_now_form_submit() so it redirects to kit page so attributes can be chosen.
    Help text update to change price handler to price alterer.
    d#499090 by justinrandell, rszrama: change the way the cart cache is rebuilt to address performance issues on login and for product kits.
    #479784 by Island Usurper: Change currency fields to numeric(16,5).

--- docs/hooks.php
+++ docs/hooks.php
@@ -1,5 +1,5 @@
 <?php
-// $Id: hooks.php,v 1.1.2.18 2009-09-21 14:34:50 islandusurper Exp $
+// $Id: hooks.php,v 1.1.2.19 2009-09-23 15:45:55 islandusurper Exp $
 
 /**
  * @file
@@ -1518,6 +1518,25 @@ function hook_uc_product_models($node) {
   return $models;
 }
 
+/**
+ * Allow modules to take action when a stock level is changed.
+ *
+ * @param $sku
+ *   The SKU whose stock level is being changed.
+ * @param $stock
+ *   The stock level before the adjustment.
+ * @param $qty
+ *   The amount by which the stock level was changed.
+ */
+function hook_uc_stock_adjusted($sku, $stock, $qty) {
+  $params = array(
+    'sku' => $sku,
+    'stock' => $stock,
+    'qty' => $qty,
+  );
+
+  drupal_mail('uc_stock_notify', 'stock-adjusted', uc_store_email_from(), language_default(), $params);
+}
 
 /**
  * Allows modules to alter items before they're added to the UC Google Analytics
